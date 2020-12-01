12091d33d6456ae3650acf042a1da52ddd6a2c21
Muzafer <muzafer.xp@gmail.com>
2020-12-01 08:04:21 +0530

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,29 @@
+print("Hello World!")
+
+#===========COMMENT=============
+#print('Hello World!')
+
+a = 10
+
+b = "ABC"
+
+#=====
+#it should not be a number in the beginning
+#it should not be a predefined key word (print, def, if, else)
+#_ special character can be used in the beginning
+#numbers can be included in middle or ending of the variable name
+
+typeof_a = type(a)
+
+print(typeof_a)
+print(type(b))
+
+#arithmatic
+
+x = 5
+y = 10
+
+z = x + y
+
+z = 2 ** 4
+print(z)
