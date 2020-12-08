6461709cee482ef566bfbdb868fe40cbb463a604
Muzafer <muzafer.xp@gmail.com>
2020-12-08 21:16:13 +0530

    day 8

--- Python/calling_module.py
+++ Python/calling_module.py
@@ -1,22 +1,10 @@
-#import my_module1
+import my_module1
 
-#from my_module1 import *
+from my_module1 import *
 
-#from my_module1 import say_hello
+from my_module1 import say_hello
 
 from my_module1 import say_hello as sa
 
 sa()
 
-#=========================BUILT-IN MODULES===============
-#1. DATETIME MODULE
-
-import datetime
-
-#current date time
-
-date = datetime.datetime.now()
-
-date = date.strftime("%x")
-
-print(date)
